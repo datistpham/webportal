c6516fabc1aea8fb6bc6071b0809d1e0dfaa9615
fptcsm <gianglamweb@gmail.com>
2024-04-26 14:50:26 +0700

    commit database

--- pages/api/connect.js
+++ pages/api/connect.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise"
 
 const connection= mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "portal",
+    host: "185.232.14.52",
+    user: "u898129453_sonama",
+    password: "Anhem12!",
+    database: "u898129453_portal",
 })
 
 export default connection
